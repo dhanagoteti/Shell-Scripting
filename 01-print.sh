df3bca152932eaa20b1355820de6931c330cc0c7
push-1 <dhana437@gmail.com>
2022-04-09 15:03:55 +0530

    added line and word with next line and tab

--- 01-print.sh
+++ 01-print.sh
@@ -7,5 +7,5 @@ echo -e "\e[31mRED\e[32mGREEN\e[33myellow\e[34mblue\e[35mMagenta\e[36mCyAN\e{0m"
 echo -e "\e[1;31mRED\e[1;32mGREEN\e[1;33myellow\e[1;34mblue\e[1;35mMagenta\e[1;36mCyAN\e{0m"
 
 
-echo -e "Line1\nLine 2"
-echo -e "Word1\tWord2"
+echo -e "Line1\nLine2"
+echo -e "Word1\t\tWord2"
