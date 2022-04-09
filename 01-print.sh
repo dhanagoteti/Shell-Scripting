2542bd786415af47b0c3b4132a8b8b98d0c70df9
push-1 <dhana437@gmail.com>
2022-04-09 15:02:38 +0530

    added line and word with next line and tab

--- 01-print.sh
+++ 01-print.sh
@@ -5,3 +5,7 @@ echo Devops Engineer
 echo -e "\e[31mRED\e[32mGREEN\e[33myellow\e[34mblue\e[35mMagenta\e[36mCyAN\e{0m"
 
 echo -e "\e[1;31mRED\e[1;32mGREEN\e[1;33myellow\e[1;34mblue\e[1;35mMagenta\e[1;36mCyAN\e{0m"
+
+
+echo -e "Line1\nLine 2"
+echo -e "Word1\tWord2"
