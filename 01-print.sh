85426455956b3f0c67eb1d478a2595e20fc3c342
push-1 <dhana437@gmail.com>
2022-04-09 14:58:13 +0530

    added color to print file more brightness and bold

--- 01-print.sh
+++ 01-print.sh
@@ -3,3 +3,5 @@
 echo this is dhana
 echo Devops Engineer
 echo -e "\e[31mRED\e[32mGREEN\e[33myellow\e[34mblue\e[35mMagenta\e[36mCyAN\e{0m"
+
+echo -e "\e[1;31mRED\e[1;32mGREEN\e[1;33myellow\e[1;34mblue\e[1;35mMagenta\e[1;36mCyAN\e{0m"
